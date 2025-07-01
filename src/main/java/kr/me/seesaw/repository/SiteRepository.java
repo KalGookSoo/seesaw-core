52f1806e32b340da4457a2d1de2a6bb234058cf7
kimdoyeob <ga.miro3721@gmail.com>
2025-07-01 22:34:18 +0900

    feat: swagger-ui REST API 추가

--- src/main/java/kr/me/seesaw/repository/SiteRepository.java
+++ src/main/java/kr/me/seesaw/repository/SiteRepository.java
@@ -1,6 +1,8 @@
 package kr.me.seesaw.repository;
 
 import kr.me.seesaw.domain.Site;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.Repository;
 
 import java.util.Optional;
@@ -10,5 +12,7 @@ public interface SiteRepository extends Repository<Site, String> {
     Site save(Site site);
 
     Optional<Site> findByDomainName(String domainName);
+
+    Page<Site> findAll(Pageable pageable);
 
 }
