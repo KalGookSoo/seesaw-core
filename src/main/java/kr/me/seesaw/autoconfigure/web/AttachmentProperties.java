c7221e7ba4862b39a7971a653611f4f8d99bf9be
Doyevskyi <ga.miro3721@gmail.com>
2025-11-02 11:59:57 +0900

    chore: 스프링부트 메타데이터 파일 추가 및 구성 정보 수정
    - datasource.properties 삭제

---
+++ src/main/java/kr/me/seesaw/autoconfigure/web/AttachmentProperties.java
@@ -0,0 +1,15 @@
+package kr.me.seesaw.autoconfigure.web;
+
+public class AttachmentProperties {
+
+    private final String filepath;
+
+    public AttachmentProperties(String filepath) {
+        this.filepath = filepath;
+    }
+
+    public String getFilepath() {
+        return filepath;
+    }
+
+}
