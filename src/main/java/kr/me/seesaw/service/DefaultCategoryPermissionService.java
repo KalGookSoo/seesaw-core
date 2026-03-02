8cb864a559c2163965e8795f0fa7d21163fd18f0
Doyevskyi <ga.miro3721@gmail.com>
2026-03-02 15:47:20 +0900

    feat: DefaultCategoryPermissionService에 로깅 및 트랜잭션 추가
    - 읽기 작업에 대해 `@Transactional(readOnly = true)` 추가
    - SLF4J 로거 도입 및 권한 검증 시 디버그 로깅 추가

--- src/main/java/kr/me/seesaw/service/DefaultCategoryPermissionService.java
+++ src/main/java/kr/me/seesaw/service/DefaultCategoryPermissionService.java
@@ -1,11 +1,21 @@
 package kr.me.seesaw.service;
 
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.acls.model.Permission;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
 public class DefaultCategoryPermissionService implements CategoryPermissionService {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
     @Override
     public boolean hasPermission(String id, Permission permission) {
+        logger.debug("인가를 검증합니다. categoryId: {}", id);
         return false;
     }
 
