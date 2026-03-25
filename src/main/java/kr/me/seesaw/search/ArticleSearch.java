8573f901394b3a00d9b901d4b32c6ea9869926d3
Doyevskyi <ga.miro3721@gmail.com>
2026-03-25 09:48:09 +0900

    feat: 상수명 수정 및 게시글 검색 모델 명세 수정

--- src/main/java/kr/me/seesaw/search/ArticleSearch.java
+++ src/main/java/kr/me/seesaw/search/ArticleSearch.java
@@ -4,9 +4,15 @@ import kr.me.seesaw.domain.vo.CategoryType;
 import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Optional;
 
 @Data
 public class ArticleSearch {
+
+    private Pageable pageable;
 
     private String categoryId;
 
@@ -17,6 +23,18 @@ public class ArticleSearch {
     private String keyField;
 
     private String keyWord;
+
+    /**
+     * 검색 조건을 UriComponentsBuilder로 반환 (Thymeleaf 링크 생성 등에서 사용)
+     */
+    public UriComponentsBuilder getUriComponentsBuilder() {
+        return UriComponentsBuilder.newInstance()
+                .queryParamIfPresent("categoryId", Optional.ofNullable(categoryId))
+                .queryParamIfPresent("categoryType", Optional.ofNullable(categoryType))
+                .queryParamIfPresent("viewType", Optional.ofNullable(viewType))
+                .queryParamIfPresent("keyField", Optional.ofNullable(keyField))
+                .queryParamIfPresent("keyWord", Optional.ofNullable(keyWord));
+    }
 
     @Getter
     @RequiredArgsConstructor
