816d231ea8365223b7685a8ea54e8a93a31f01fc
Doyevskyi <ga.miro3721@gmail.com>
2026-06-20 21:46:32 +0900

    chore: 오탈자 수정

--- src/main/java/kr/me/seesaw/command/MoveArticleCommand.java
+++ src/main/java/kr/me/seesaw/command/MoveArticleCommand.java
@@ -8,7 +8,7 @@ import jakarta.validation.constraints.NotNull;
 @Schema(description = "게시글 이동 커맨드")
 public record MoveArticleCommand(
         @Parameter(description = "대상 카테고리 식별자", required = true)
-        @Schema(description = "대상 카테고리 식별자", example = "UUID")
+        @Schema(description = "대상 카테고리 식별자", example = "8f14e45f-ea9d-4b1c-a3a4-12c4b2a9c001")
         @NotBlank
         @NotNull
         String categoryId
