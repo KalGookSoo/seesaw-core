0706c92ff420de628c46443d647f85126f3597af
Doyevskyi <ga.miro3721@gmail.com>
2026-06-29 18:12:53 +0900

    fix: 게시글 조회 이벤트 핸들러 트랜잭션 설정 변경

--- src/main/java/kr/me/seesaw/event/listener/ArticleViewEventListener.java
+++ src/main/java/kr/me/seesaw/event/listener/ArticleViewEventListener.java
@@ -5,12 +5,11 @@ import kr.me.seesaw.domain.View;
 import kr.me.seesaw.event.ArticleViewedEvent;
 import kr.me.seesaw.repository.ViewRepository;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @Slf4j
 @Component
@@ -22,8 +21,8 @@ public class ArticleViewEventListener {
         this.viewRepository = viewRepository;
     }
 
-    @EventListener
     @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT, fallbackExecution = true)
     public void onArticleViewed(ArticleViewedEvent event) {
         log.debug("게시글 조회 이벤트 처리: {}", event);
         View view = new View();
